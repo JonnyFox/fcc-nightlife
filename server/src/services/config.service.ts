829cc31c740ccfcd8e6e83f45bb35036a94716d5
jfox <fox.jonny@gmail.com>
2017-05-19 00:34:39 +0200

    remove config

--- server/src/services/config.service.ts
+++ server/src/services/config.service.ts
@@ -5,10 +5,9 @@ export class ConfigService {
     public env = process.env.NODE_ENV || 'development';
     public port = process.env.PORT || 8999;
     public db = {
-        uri: process.env.MONGODB_URI || 'mongodb://admin:admin@ds157320.mlab.com:57320/nightlife-app'
+        uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/nightlife-app'
     };
     public googleApi = {
-        // key: process.env.GOOGLE_API_KEY || 'AIzaSyAgrZgFAVJz2mFjoTY5PaP_1yAG9lV5WrU'
-        key: 'AIzaSyAgrZgFAVJz2mFjoTY5PaP_1yAG9lV5WrU'
+        key: process.env.GOOGLE_API_KEY
     };
 }
